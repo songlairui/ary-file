67eda73178e6b7ef12f1bd3c6c6d070ca739e3e0
songlairui <cnre@live.cn>
2019-04-14 01:51:17 +0800

    add gql serve

--- packages/fed/.eslintrc.js
+++ packages/fed/.eslintrc.js
@@ -1,18 +1,33 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended',
     '@vue/typescript'
   ],
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'graphql/template-strings': [
+      'error',
+      {
+        env: 'literal',
+        projectName: 'app'
+      }
+    ]
   },
+
   parserOptions: {
     parser: '@typescript-eslint/parser'
-  }
+  },
+
+  plugins: [
+    'graphql'
+  ]
 }
--- packages/fed/src/main.ts
+++ packages/fed/src/main.ts
@@ -3,11 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import { createProvider } from './vue-apollo'
+import VueRx from 'vue-rx';
+
+Vue.use(VueRx);
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
